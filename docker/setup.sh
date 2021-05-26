9ec506acee3e6ac5210977e2d59180fa640652e8
Msch0150 <37273467+Msch0150@users.noreply.github.com>
2021-05-26 10:25:29 +0200

    Additional comments

--- docker/setup.sh
+++ docker/setup.sh
@@ -16,3 +16,10 @@ unzip master.zip && sudo chown -R 8983:8983 numishare-master/solr-home/ && \
 cd numishare-master/docker
 
 docker-compose up
+
+# Using the current docker-compose.xml you might want to try:
+#
+# Numishare: http://localhost:8081/orbeon/numishare/admin
+# Exist DB : http://localhost:8080
+# Solr     : http://localhost:8983/
+# Loris    : http://localhost:10206/
