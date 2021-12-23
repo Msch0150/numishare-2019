afaae0a08808f14f8526cb79395789564c7cc11b
Msch0150 <37273467+Msch0150@users.noreply.github.com>
2021-12-23 10:36:39 +0100

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,6 +2,7 @@
 mkdir -p ${HOME}/data/docker-loris-data/images
 mkdir -p ${HOME}/data/docker-solr-data
 sudo chown -R 8983:8983 ${HOME}/data/docker-solr-data
+mkdir -p ${HOME}/data/docker-fuseki-data
 
 # Cleanup
 cd ${HOME} && \
@@ -23,8 +24,10 @@ docker-compose up
 
 # Using the current docker-compose.xml you might want to try:
 #
-# Numishare: http://localhost:8081/orbeon/numishare/admin/
-# Exist DB : http://localhost:8080
-# Solr     : http://localhost:8983/
-# Apache   : http://localhost
+# Numishare: http://localhost:10200/orbeon/numishare/admin/
+# Solr     : http://localhost:10202/
+# Exist DB : http://localhost:10204
 # Loris    : http://localhost:10206/
+# Fuseki   : http://localhost:10208/  (username:password displayed in console of Docker)
+# Apache   : http://localhost
+
